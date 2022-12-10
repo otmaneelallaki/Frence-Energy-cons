0b3628e70f76b9acabe74c0ded92c283b118fafd
otmaneelllaki <otmane.allaki1@gmail.com>
2022-12-10 03:29:24 +0100

    creat modules and submod

---
+++ fec/src/louad_db/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+url = "https://raw.githubusercontent.com/otmaneelallaki/HAX712X-DOS/main/Project/Prediction/Dataset/DataTrining.csv"
+path_target = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "..", "data", "DataTrining.csv"
+)
